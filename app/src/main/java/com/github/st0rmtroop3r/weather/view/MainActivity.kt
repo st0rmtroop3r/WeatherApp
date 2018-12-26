94316c89b76ca64b753f5b06160dda0a235c97a1
Ihor <i.p.kryvosheia@gmail.com>
2018-12-26 13:03:00 +0200

    migrate to AndroidX

--- app/src/main/java/com/github/st0rmtroop3r/weather/view/MainActivity.kt
+++ app/src/main/java/com/github/st0rmtroop3r/weather/view/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.github.st0rmtroop3r.weather.view
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import com.github.st0rmtroop3r.weather.R
 import com.github.st0rmtroop3r.weather.di.DaggerAppComponent
--- app/src/main/java/com/github/st0rmtroop3r/weather/view_model/MainViewModel.kt
+++ app/src/main/java/com/github/st0rmtroop3r/weather/view_model/MainViewModel.kt
@@ -1,6 +1,6 @@
 package com.github.st0rmtroop3r.weather.view_model
 
-import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
