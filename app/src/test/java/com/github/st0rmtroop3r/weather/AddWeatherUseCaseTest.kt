b63ef6177900243e72d1c8929abce618b7b720b4
Ihor <i.p.kryvosheia@gmail.com>
2019-02-19 14:55:50 +0200

    add test for AddWeatherUseCase

---
+++ app/src/test/java/com/github/st0rmtroop3r/weather/AddWeatherUseCaseTest.kt
@@ -0,0 +1,66 @@
+package com.github.st0rmtroop3r.weather
+
+import com.github.st0rmtroop3r.weather.model.entities.Weather
+import com.github.st0rmtroop3r.weather.model.repository.WeatherRepository
+import com.github.st0rmtroop3r.weather.viewmodel.usecase.AddWeatherUseCase
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.doThrow
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class AddWeatherUseCaseTest {
+
+    @Mock
+    lateinit var weather: Weather
+
+    @Mock
+    lateinit var weatherRepository: WeatherRepository
+
+    @InjectMocks
+    lateinit var addWeatherUseCase: AddWeatherUseCase
+
+    @Test
+    fun test_execution_with_non_null_param() {
+
+        var result: Any? = Object()
+        runBlocking {
+            addWeatherUseCase.execute(weather, this) { result = it }
+        }
+        assert(result == null)
+    }
+
+    @Test
+    fun test_execution_with_null_param() {
+
+        var result: Any? = Object()
+        runBlocking {
+            addWeatherUseCase.execute(null, this) { result = it }
+        }
+        assert(result is NullPointerException)
+    }
+
+    @Test
+    fun test_execution_with_non_null_param_without_callback() {
+
+        runBlocking {
+            addWeatherUseCase.execute(weather, this)
+        }
+        assert(true)
+    }
+
+    @Test
+    fun test_execution_if_exception_occured_without_callback() {
+
+        doThrow(UnsupportedOperationException()).`when`(weatherRepository).addWeather(weather)
+
+        runBlocking {
+            addWeatherUseCase.execute(weather, this)
+        }
+        assert(true)
+    }
+}
+
